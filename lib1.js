538bf7179c071a27c13198d8913afd40e131c583
ftima13 <utp0156952@alumno.utpuebla.edu.mx>
2025-09-27 23:53:07 -0600

    Update lib1.js intento3

--- lib1.js
+++ lib1.js
@@ -1,40 +1,37 @@
-// lib.js
-// http://localhost/pwasd25/index.html?n=2&d=5
 const params = new URLSearchParams(window.location.search);
-const n = params.get('n');
-const d = params.get('d');
+const n = parseInt(params.get('n'));
+const d = parseInt(params.get('d'));
 
 class Quickchart {
-    constructor(d) {
+    constructor(n, d) {
+        this.n = n;
         this.d = d;
     }
 
-    // Crear una cadena de 1's para dividir el pastel en partes iguales
     crearCadunos() {
         let cadunos = "";
-        // Cambiar el bucle para que vaya de 0 a d
-        for (var i = 0; i < this.d; i++) {
-            cadunos += "1,";  // Cada "1" representa una parte del pastel
+        for (let i = 0; i < this.n; i++) {
+            cadunos += "1,";  // part visible (marcada)
         }
-        cadunos = cadunos.slice(0, -1);  // Eliminar la última coma extra
-        return cadunos;
+        for (let i = 0; i < this.d - this.n; i++) {
+            cadunos += "0.001,";  // part invisible but existing
+        }
+        return cadunos.slice(0, -1);
     }
 
-    // Generar la URL del gráfico
     generarSrcImg() {
-        let url = "https://quickchart.io/chart?cht=p3&chd=t:" + this.crearCadunos() 
-                 + "&chs=500x250&chl=" + "1/" + this.d;  // Mostrar "1/d" como etiqueta
+        const data = this.crearCadunos();
+
+        // Creamos etiquetas: solo la parte marcada (n/d), el resto vacío
+        const labels = ` ${this.n}/${this.d}` + ",".repeat(this.d - 1);
+
+        const url = `https://quickchart.io/chart?cht=p3&chd=t:${data}&chs=500x250&chl=${labels}`;
         return url;
     }
 }
 
-// Crear el gráfico con el valor de d proporcionado en la URL
-let q = new Quickchart(d);
+let q = new Quickchart(n, d);
 document.getElementById("contenido").innerHTML = '<img src="' + q.generarSrcImg() + '" />';
 
-// '<img src="https://quickchart.io/chart?cht=p3&chd=t:1,1,1,1&chs=500x250&chl=1/4">';
-// q.generarSrcImg();
-
-// "<h1>Adios</h1>";
 
 
