f2317eef289803ca8d0c13e7a9336d3ce9c8ace0
ftima13 <utp0156952@alumno.utpuebla.edu.mx>
2025-09-27 23:26:56 -0600

    Update lib1.js

--- lib1.js
+++ lib1.js
@@ -11,18 +11,20 @@ class Quickchart {
     crearCadunos() {
         let cadunos = "";
         for (let i = 0; i < this.n; i++) {
-            cadunos += "1,";
+            cadunos += "1,";  // part visible (marcada)
         }
         for (let i = 0; i < this.d - this.n; i++) {
-            cadunos += "0,";
+            cadunos += "0.001,";  // part invisible but existing
         }
-        cadunos = cadunos.slice(0, -1);
-        return cadunos;
+        return cadunos.slice(0, -1);
     }
 
     generarSrcImg() {
         const data = this.crearCadunos();
-        const labels = `${this.n}/${this.d}` + ",".repeat(this.d - 1);
+
+        // Creamos etiquetas: solo la parte marcada (n/d), el resto vacío
+        const labels = ` ${this.n}/${this.d}` + ",".repeat(this.d - 1);
+
         const url = `https://quickchart.io/chart?cht=p3&chd=t:${data}&chs=500x250&chl=${labels}`;
         return url;
     }
@@ -31,9 +33,6 @@ class Quickchart {
 let q = new Quickchart(n, d);
 document.getElementById("contenido").innerHTML = '<img src="' + q.generarSrcImg() + '" />';
 
-// '<img src="https://quickchart.io/chart?cht=p3&chd=t:1,1,1,1&chs=500x250&chl=1/4">';
-// q.generarSrcImg();
 
-// "<h1>Adios</h1>";
 
 
