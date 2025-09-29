856af93db506475d91d015c55480375984ed8dcd
ftima13 <utp0156952@alumno.utpuebla.edu.mx>
2025-09-29 08:58:07 -0600

    Update lib1.js

--- lib1.js
+++ lib1.js
@@ -1,41 +1,33 @@
-// Obtener parámetros de URL
+// lib.js
+// http://localhost/pwasd25/index.html?n=1&d=4
 const params = new URLSearchParams(window.location.search);
-const n = parseInt(params.get('n'));
-const d = parseInt(params.get('d'));
+const n = params.get('n');
+const d = params.get('d');
 
-if (!isNaN(n) && !isNaN(d)) {
-    class Quickchart {
-        constructor(n, d) {
-            this.n = n;
-            this.d = d;
-        }
-
-        crearCadunos() {
-            let cad = Array(this.d).fill(1); // todas las rebanadas iguales
-            return cad.join(',');
-        }
-
-        generarEtiquetas() {
-            let etiquetas = [];
-            for (let i = 0; i < this.d; i++) {
-                if (i < this.n) {
-                    etiquetas.push(this.n + "/" + this.d); // poner la fracción en las primeras n
-                } else {
-                    etiquetas.push(""); // las demás sin etiqueta
-                }
-            }
-            return etiquetas.join('|');
-        }
-
-        generarSrcImg() {
-            let url = "https://quickchart.io/chart?cht=p3&chd=t:" + this.crearCadunos()
-                + "&chs=500x250&chl=" + this.generarEtiquetas();
-            return url;
-        }
+class Quickchart {
+    constructor(d) {
+        this.d = d;
     }
 
-    let q = new Quickchart(n, d);
-    document.getElementById("contenido").innerHTML = '<img src="' + q.generarSrcImg() + '" />';
-} else {
-    document.getElementById("contenido").innerHTML = "<p>Por favor, agrega n y d en la URL</p>";
+    crearCadunos() {
+        let cadunos = "";
+        for (var i = 1; i < this.d; i++) {
+            cadunos += "1,";
+        }
+        cadunos = cadunos.slice(0, -1);
+        return cadunos;
+    }
+
+    generarSrcImg() {
+        let url = "https://quickchart.io/chart?cht=p3&chd=t:" + this.crearCadunos() 
+                 + "&chs=500x250&chl=" + "1/" + this.d;
+        return url;
+    }
 }
+
+let q = new Quickchart(d);
+document.getElementById("contenido").innerHTML = '<img src="' + q.generarSrcImg() + '" />';
+// '<img src="https://quickchart.io/chart?cht=p3&chd=t:1,1,1,1&chs=500x250&chl=1/4">';
+// q.generarSrcImg();
+
+// "<h1>Adios</h1>";
